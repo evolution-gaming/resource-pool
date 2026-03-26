afffc042d872f17e42c85a3694c93111b39a5504
Scala Steward <scala_steward@virtuslab.com>
2026-03-26 18:39:42 +0000

    Update scalatest to 3.2.20

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,6 +1,6 @@
 import sbt.*
 
 object Dependencies {
-  val ScalaTest = "org.scalatest" %% "scalatest" % "3.2.19" % Test
+  val ScalaTest = "org.scalatest" %% "scalatest" % "3.2.20" % Test
   val CatsEffect = "org.typelevel" %% "cats-effect" % "3.7.0"
 }
